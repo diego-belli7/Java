4d3f11ce4c9f2744b68a8c748952c173db2fec5e
Diego Belli <128156867+diego-belli7@users.noreply.github.com>
2024-10-31 08:16:37 -0300

    .

---
+++ main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("Esse é o Screen Match");
+      System.out.println("Era do gelo");
+      
+      int ano = 2022;
+      boolean incluidoNoPlano = true;
+      double notaFilme = 4.8;
+      
+      System.out.println("Ano de lancamento: " + ano);
+     
+      
+  }
+}
